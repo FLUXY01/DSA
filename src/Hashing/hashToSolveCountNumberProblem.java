ed1f55633314d782e6fa046935a127fb01fa0482
Vimanyu Jain <vimanyujain07@gmail.com>
2026-01-28 01:07:27 +0530

    chore: solved hashToSolveCountCharProblem

--- src/Hashing/hashToSolveCountNumberProblem.java
+++ src/Hashing/hashToSolveCountNumberProblem.java
@@ -2,7 +2,7 @@ package Hashing;
 
 import java.util.Scanner;
 
-public class useHashToSolve {
+public class hashToSolveCountNumberProblem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
