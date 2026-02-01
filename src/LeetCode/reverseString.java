355644e0f1aee702faea5297c6a7e21c026bd55a
Vimanyu Jain <vimanyujain07@gmail.com>
2026-02-02 00:04:07 +0530

    chore: solved reverseStringExceptSpecialChar

--- src/LeetCode/reverseString.java
+++ src/LeetCode/reverseString.java
@@ -2,15 +2,25 @@ package LeetCode;
 
 public class reverseString {
     public static void main(String[] args) {
-        String s = "abcde";
+        String s = "a@#bcde";
         char [] arr = s.toCharArray();
         int n = arr.length;
-        for(int i = 0; i < n/2; i++){
-            char temp = arr[i];
-            arr[i] = arr[n-i-1];
-            arr[n-i-1] = temp;
+        int left = 0;
+        int right = n-1;
+        while (left<right){
+            while (left<right && !Character.isLetterOrDigit(arr[left])){
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(arr[right])){
+                right--;
+            }
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
         }
-        s = new String(arr);
-        System.out.println(s);
+        String result = new String(arr);
+        System.out.println(result);
     }
 }
