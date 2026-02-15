be7fe2b2594347da4e8a4b1b1e9a91ede5c07f7f
Vimanyu Jain <vimanyujain07@gmail.com>
2026-02-15 22:01:52 +0530

    chore: solved FindDuplicateFromSortedArray

--- src/ProblemOnArrays/Easy/FindDuplicateFromSortedArray.java
+++ src/ProblemOnArrays/Easy/FindDuplicateFromSortedArray.java
@@ -4,8 +4,9 @@ import java.util.HashSet;
 
 public class FindDuplicateFromSortedArray {
     public static void main(String[] args) {
-        int [] arr = {1,1,2};
-        System.out.println(findDuplicateFromSortedArray(arr));
+        int [] arr = {0,1,1,1,2,3,3,3};
+       // System.out.println(findDuplicateFromSortedArray(arr));
+        System.out.println(optimized(arr));
     }
     public static int findDuplicateFromSortedArray(int[]arr){
         int count = 0;
@@ -19,4 +20,17 @@ public class FindDuplicateFromSortedArray {
         }
         return count;
     }
+    public static int optimized(int[]arr){
+        int count = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[count]) {
+                count++;
+                arr[count] = arr[i];
+            }
+        }
+
+        return count + 1;
+    }
+
 }
